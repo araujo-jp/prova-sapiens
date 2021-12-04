0c944c966d104d7ee83f7339a5fc7183a49cce6e
Joao Paulo Araujo <paulointer077@gmail.com>
2021-12-04 12:39:22 -0400

    feat: validating sign in form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,38 @@
 import { Flex, Stack, Button } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import { useForm, SubmitHandler} from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
+import { useRouter } from 'next/router';
 
 import { Logo } from '../components/logo'
 import { Input } from '../components/form';
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  const { register, handleSubmit, formState } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async values => {
+    console.log(values)
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    if (formState.errors) {
+      router.push('/dashboard');
+    }
+  };
+
   return (
     <Flex width="100vw" height="100vh" align="center" justify="center" direction="column">
 
@@ -12,13 +40,24 @@ const Home: NextPage = () => {
 
         <Logo width="auto" fontSize="6xl" />
 
-        <Flex as="form" width="100%" maxWidth={400} bg="gray.800" padding="8" borderRadius={8} flexDir="column">
+        <Flex as="form" width="100%" maxWidth={400} bg="gray.800" padding="8" borderRadius={8} flexDir="column" onSubmit={handleSubmit(handleSignIn)}>
 
           <Stack spacing="5">
 
-            <Input label="E-mail" name="email" type="email" tabIndex={1} />
-
-            <Input label="Senha" name="password" type="password" tabIndex={2} />
+            <Input
+              className="email"
+              label="E-mail"
+              type="email"
+              error={formState.errors.email}
+              {...register('email')}
+            />
+            <Input
+              className="password"
+              label="Senha"
+              type="password"
+              error={formState.errors.password}
+              {...register('password')}
+            />
 
           </Stack>
 
@@ -27,6 +66,7 @@ const Home: NextPage = () => {
             mt="6"
             colorScheme="orange"
             size="lg"
+            isLoading={formState.isSubmitting}
           >
             Entrar
           </Button>
